20f0539e04c20d864f5fe4f28da09b6672962777
Helenagapr <helenagp05@gmail.com>
2025-07-18 15:00:19 +0200

    feat: Add WhatsApp button component with styling for chat functionality

--- artbrujula/src/pages/App.tsx
+++ artbrujula/src/pages/App.tsx
@@ -3,6 +3,7 @@ import Header from "../components/header";
 import { AppRouter } from "../routes/AppRouter";
 import { BrowserRouter } from "react-router-dom";
 import style from '../styles/global.module.scss'; 
+import WhatsAppButton from "../components/chat";
 
 export default function App(){
     return (
@@ -13,6 +14,7 @@ export default function App(){
                     <main className={style.container}>
                         <AppRouter />
                     </main>
+                    <WhatsAppButton />
                 </BrowserRouter>  
             </Suspense> 
         </>
